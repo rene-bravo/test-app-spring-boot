8b63fd7657b04bb1fba638bb2bc83e836476bc02
Rene BA <amador_19927@hotmail.com>
2022-08-15 11:43:24 -0500

    update components for send info from html frontend

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export class TestDTO{
+  constructor(public arrayOne: string, public arrayTwo: string){}
+}
+
+export class ServiceDTO{
   constructor(public arrayOne: string[], public arrayTwo: string[]){}
 }
 
@@ -12,7 +16,7 @@ export class HttpClientService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public countRepeatedNumberInArray(testDTO: TestDTO){
-    return this.httpClient.post<TestDTO>("http://localhost:8080/api/testArray", testDTO);
+  public countRepeatedNumberInArray(serviceDTO: ServiceDTO){
+    return this.httpClient.post<ServiceDTO>("http://localhost:8080/api/testArray", serviceDTO);
   }
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientService, TestDTO} from '../service/http-client.service';
+import { HttpClientService, TestDTO, ServiceDTO} from '../service/http-client.service';
 
 @Component({
   selector: 'app-test',
@@ -8,14 +8,16 @@ import { HttpClientService, TestDTO} from '../service/http-client.service';
 })
 export class TestComponent implements OnInit {
 
-  testDTO: TestDTO = new TestDTO(["1","2","3","4","5"],["1","6","7","8","9"]);
+  testDTO: TestDTO = new TestDTO("","");
+  serviceDTO: ServiceDTO = new ServiceDTO([],[]);
 
   constructor(private httpClientService: HttpClientService) {}
 
   ngOnInit(): void {}
 
   countRepeatedNumberInArray(): void {
-    this.httpClientService.countRepeatedNumberInArray(this.testDTO)
+    this.serviceDTO = new ServiceDTO(this.testDTO.arrayOne.split(''),this.testDTO.arrayTwo.split(''));
+    this.httpClientService.countRepeatedNumberInArray(this.serviceDTO)
         .subscribe( data => {
           alert("results from back");
           console.log("response from back: " +data);
